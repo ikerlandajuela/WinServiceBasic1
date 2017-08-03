f169a53623564030095a0c5d75656a23ef49d1b2
ikerlandajuela <i.landajuela@koopera.local>
2017-08-03 09:38:20 +0200

    Versión básica servicio Windows

---
+++ WinServiceBasic1/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinServiceBasic1
+{
+    public partial class MyNewService : ServiceBase
+    {
+        public MyNewService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+    }
+}
